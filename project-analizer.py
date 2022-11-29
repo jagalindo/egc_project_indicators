2d652326c6d15906f817384b5c1640c60d3950f2
José A. Galindo <jagalindo@us.es>
2022-11-30 00:29:37 +0100

    updating with objects and dataclases

--- project-analizer.py
+++ project-analizer.py
@@ -1,28 +1,60 @@
 #!/bin/python
 from git import Repo
+from typing import List
 import openpyxl
-  
-# load excel with its path
-wrkbk = openpyxl.load_workbook("Libro1.xlsx")
-sh = wrkbk.active
-  
-# iterate through excel and display data
-for i in range(2, sh.max_row+1):
-    print("Nombre: ", sh.cell(row=i, column=1).value)
-    print("URL: ", sh.cell(row=i, column=2).value)
-    print("Ramas: ")
-    ramas=sh.cell(row=i, column=3).value.split(",")
-    for rama in ramas:
-        print("Rama: ", rama)
-    users=sh.cell(row=i, column=4).value.split("\n")
-    for user in users:
-        user_token=user.split("->")
-        print("User: ", user_token[0])
-        user_tokens=user_token[1].split(",")
-        for token in user_tokens:
-            print("Token: ", token.strip())
+
+class User:
+    def __init__(self, name:str, tokens:List[str]):
+        self.name = name
+        self.tokens = tokens  
+        self.loc = 0
+        self.commits = 0
+
+    def __str__(self):
+        return f"Name: {self.name}, Tokens: {self.tokens}"
+
+class Project:
+    def __init__(self, name:str, url:str, branches:List[str], users:List[User]):
+        self.name = name
+        self.url = url
+        self.branches = branches
+        self.users = users
+        self.total_loc = 0
+        self.total_commits = 0
         
+    def __str__(self):
+        return f"Name: {self.name}, Url: {self.url}, Branches: {self.branches}, Users: {self.users}"
+
+
+def main():
+    projects = get_projects("Libro1.xlsx")
+    for project in projects:
+        print(project)
+    
+    
+
+def get_projects(excel_file:str) -> List[Project]:
+    projects=[]
+    # load excel with its path
+    wrkbk = openpyxl.load_workbook(excel_file)
+    sh = wrkbk.active
+    
+    # iterate through excel and display data
+    for i in range(2, sh.max_row+1):
+        project=Project(sh.cell(row=i, column=1).value.strip(),sh.cell(row=i, column=2).value.strip(),[item.strip() for item in sh.cell(row=i, column=3).value.split(",")],[])
+        users=sh.cell(row=i, column=4).value.split("\n")
+        for user in users:
+            u = User(user.split("->")[0].strip(),[item.strip for item in user.split("->")[1].split(",")])
+            project.users.append(u)
+        projects.append(project)
+    return projects
+
+def process_project(project:Project):
+    pass
 
 def git_clone(url, path):
     # clone the repo
     cloned_repo = repo.clone(url, path)
+
+if __name__ == "__main__":
+    main()
